85184b12a55fcb9d0fc734e5ef1d5a3dc6023d03
Julio Mota <juliocesarmotapereira@gmail.com>
2023-01-23 21:35:01 -0300

    add local storage

--- README.md
+++ README.md
@@ -5,4 +5,7 @@ Material de estudos Programa ONE da Oracle
 
 - HTML: colocar calendário input type datetime-local
 - JS DOM: percorrer o docuemnto querySelector()
+- Padronizar a data com https://momentjs.com/ e https://cdnjs.com/
+- API de Armazenamento na Web https://developer.mozilla.org/pt-BR/docs/Web/API/Web_Storage_API
+- JSON https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON
 - 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
 import BotaoConclui from '../js/componentes/concluiTarefa.js'
 import BotaoDeleta from '../js/componentes/deletaTarefa.js'
 
-const criarTarefa = (evento) => {
+// MANIPULANDO O ARRAY
+let tarefas = []
 
+const handleNovoItem = (evento) => {
     evento.preventDefault()
 
     const lista = document.querySelector('[data-list]')
@@ -11,22 +13,41 @@ const criarTarefa = (evento) => {
 
     // CALENDÁRIO
     const calendario = document.querySelector('[data-form-date]')
-    const data = calendario.value
-    console.log(data)
+    const data = moment(calendario.value)
 
+    const dataFormatada = data.format('DD/MM/YYYY')
+
+    const dados = {
+        valor,
+        dataFormatada
+    }
+
+    const criaTarefa = criarTarefa(dados)
+
+    // MANIPULANDO O ARRAY
+    tarefas.push(dados)
+
+    lista.appendChild(criaTarefa)
+
+    // ARMAZENANDO DADOS
+    // LOCAL STORAGE
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+    input.value = " "
+}
+
+const criarTarefa = ({ valor, dataFormatada }) => {
     const tarefa = document.createElement('li')
     tarefa.classList.add('task')
-    const conteudo = `<p class="content">${valor}</p>`
+    const conteudo = `<p class="content">${dataFormatada} * ${valor}</p>`
 
     tarefa.innerHTML = conteudo
 
     tarefa.appendChild(BotaoConclui())
     tarefa.appendChild(BotaoDeleta())
-    lista.appendChild(tarefa)
-    input.value = " "
 
+    return tarefa
 }
 
 const novaTarefa = document.querySelector('[data-form-button]')
 
-novaTarefa.addEventListener('click', criarTarefa)
+novaTarefa.addEventListener('click', handleNovoItem)
